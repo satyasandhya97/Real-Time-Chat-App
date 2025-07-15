9545daf238a901c5d4203c34a7e1ddb294cb257e
satyasandhya97 <satyasandhyabiswal97@gmail.com>
2025-07-15 17:57:15 +0530

    successfully connected the redis and also add user routes

--- Backend/user/src/index.ts
+++ Backend/user/src/index.ts
@@ -1,12 +1,25 @@
 import express from 'express';
 import dotenv from "dotenv";
 import connectDb from './config/db';
+import { createClient } from 'redis';
+import userRoutes from './routes/user';
 
 dotenv.config();
 
 connectDb();
 
+export const redisClient = createClient({
+  url: process.env.REDIS_URL
+})
+
+redisClient
+  .connect()
+  .then(() => console.log("connented to redis"))
+  .catch(console.error)
+
 const app = express();
+app.use("api/v1", userRoutes);
+
 const port = process.env.PORT;
 
 app.listen(port, ()=> {
