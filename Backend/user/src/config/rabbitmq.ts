62a14d659e2f458f770b590072ad7e6de9f6b8d6
satyasandhya97 <satyasandhyabiswal97@gmail.com>
2025-07-16 23:03:19 +0530

    add the publishToQueue function is used to send messages to a RabbitMQ queue

--- Backend/user/src/config/rabbitmq.ts
+++ Backend/user/src/config/rabbitmq.ts
@@ -17,7 +17,20 @@ export const connnectRabbitMQ = async() => {
 
         console.log("👍👍 connected to rabbitmq");
     } catch (error) {
-        console.log("Failed to connect to rabbitmq", error);
-        
+        console.log("Failed to connect to rabbitmq", error);  
     }
+}
+
+
+export const publishToQueue = async(queueName: string, message: any)=> {
+    if(!channel){
+        console.log("Rabbit channel is not initalized");
+        return;
+    }
+
+    await channel.assertQueue(queueName, {durable: true});
+
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)),{
+        persistent: true,
+    });
 }
