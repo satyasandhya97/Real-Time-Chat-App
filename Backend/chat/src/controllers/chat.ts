b23609cc257de89d8069b4d9afe17fd9e48c3973
satyasandhya97 <satyasandhyabiswal97@gmail.com>
2025-07-20 00:37:53 +0530

    Add the messages schema then to create the getAllChats API

--- Backend/chat/src/controllers/chat.ts
+++ Backend/chat/src/controllers/chat.ts
@@ -1,40 +1,91 @@
 import TryCatch from "../config/TryCatch";
 import { AuthenticatedRequest } from "../middlewares/isAuth";
 import { Chat } from "../models/chats";
+import { Message } from "../models/messages";
+import axios from "axios";
 
-export const createNewChat = TryCatch(async(req: AuthenticatedRequest, res)=>{
+export const createNewChat = TryCatch(async (req: AuthenticatedRequest, res) => {
     const userId = req.user?._id;
     const { otherUserId } = req.body;
 
-    if(!otherUserId){
+    if (!otherUserId) {
         res.status(400).json({
             message: "Other userId is required",
         });
         return;
     }
-    
+
     const existingChat = await Chat.findOne({
-        users: {$all:[userId, otherUserId], $size: 2}
+        users: { $all: [userId, otherUserId], $size: 2 }
     })
-    
-    if(existingChat){
+
+    if (existingChat) {
         res.json({
-            message : "Chat already exist",
+            message: "Chat already exist",
             chatId: existingChat._id,
         });
         return;
     }
 
     const newChat = await Chat.create({
-        users:[userId, otherUserId],
+        users: [userId, otherUserId],
     })
 
     res.status(201).json({
         message: "New Chat created",
         chatId: newChat._id,
     });
+});
 
-    
+export const getAllChats = TryCatch(async (req: AuthenticatedRequest, res) => {
+    const userId = req.user?._id;
+
+    if (!userId) {
+        res.status(400).json({
+            message: "UserId missing",
+        });
+        return;
+    }
+
+    const chats = await Chat.find({ users: userId }).sort({ updateAt: -1 });
+
+    const chatWithUserData = await Promise.all(
+        chats.map(async (chat) => {
+            const otherUserId = chat.users.find((id) => id !== userId);
+
+            const unseenCount = await Message.countDocuments({
+                chatId: chat._id,
+                sender: { $ne: userId },
+                seen: false,
+            });
+
+            try {
+                const { data } = await axios.get(`${process.env.USER_SERVICE}/api/v1/user/${otherUserId}`)
+                return {
+                    user: data,
+                    chat: {
+                        ...chat.toObject(),
+                        latestMessage: chat.latestMessage || null,
+                        unseenCount,
+                    },
+                };
+            } catch (error) {
+                console.log(error);
+                return {
+                    user: { _id: otherUserId, name: "Unknown User" },
+                    chat: {
+                        ...chat.toObject(),
+                        latestMessage: chat.latestMessage || null,
+                        unseenCount,
+                    },
+                };
+            }
 
 
-})
+        })
+    )
+
+    res.json({
+        chats: chatWithUserData,
+    });
+});
--- Backend/chat/src/routes/chat.ts
+++ Backend/chat/src/routes/chat.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createNewChat } from '../controllers/chat';
+import { createNewChat, getAllChats } from '../controllers/chat';
 import { isAuth } from '../middlewares/isAuth';
 
 const router = express.Router();
 
 router.post("/chat/new", isAuth, createNewChat)
+router.get("/chat/all", isAuth, getAllChats);
 
 export default router;
