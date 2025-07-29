0f51b0ffb641f8a8647bfcf5b6d1e636a06a3b76
satyasandhya97 <satyasandhyabiswal97@gmail.com>
2025-07-29 11:37:53 +0530

    Add the chat interface and add other require variables

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,11 +1,37 @@
 "use client";
-import { useAppData } from '@/context/AppContext'
-import React, { useEffect } from 'react'
+import { useAppData, User } from '@/context/AppContext'
+import React, { useEffect, useState } from 'react'
 import Loading from "@/components/loading";
 import { useRouter } from 'next/navigation';
 
+export interface message{
+  _id: string;
+  chatId: string;
+  sender: string;
+  text?: string;
+  image?: {
+    url: string;
+    publicId: string;
+  };
+  messageType: "text" | "image"
+  seen : boolean;
+  seenAt?: string;
+  createdAt: string;
+}
+
 const ChatApp = () => {
-  const {loading, isAuth } = useAppData();
+  const {loading, isAuth , logOutUser, chats, user:loggedInUser,
+    users,
+    fetchChats,
+    setChats,
+  } = useAppData();
+  
+  const [selectedUser, setSelectedUser] = useState<string | null>(null)
+  const [message, setMessage] = useState("");
+  const[siderbarOpen, setSiderbarOpen] = useState(false)
+  const[messages, setMessages] = useState<message[] | null>(null)
+  const[user, setUser] = useState<User | null>(null)
+  const[ showAllUser, setShowAllUser ] = useState(false)
 
   const router = useRouter();
   
