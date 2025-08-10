77743658515db9cd0817fbdcbbe2abfcc7977da5
satyasandhya97 <satyasandhyabiswal97@gmail.com>
2025-08-10 22:04:37 +0530

    Initial commit

--- frontend/src/components/chatHeader.tsx
+++ frontend/src/components/chatHeader.tsx
@@ -1,8 +1,24 @@
+import { User } from '@/context/AppContext';
+import { Menu } from 'lucide-react'
 import React from 'react'
 
-const chatHeader = () => {
+interface chatHeaderProps{
+  user: User | null;
+  setSidebarOpen: (open: boolean) => void;
+  isTyping: boolean;
+}
+
+const chatHeader =( {user, setSidebarOpen, isTyping} :chatHeaderProps)  => {
   return (
-    <div>chatHeader</div>
+    <>
+      {/* mobile menu toggle */}
+      <div className="sm:hidden fixed top-4 right-4 z-30">
+        <button className='p-3 bg-gray-800 rounded-lg hover:bg-gray-700
+         transition-colors'>
+          <Menu className='w-5 h-5 text-gray-200' />
+         </button>
+      </div>
+    </>
   )
 }
 
